98ee782d1095781db49e97d351abc55bd519c629
Graham Booker <gbooker@plexapp.com>
2024-08-22 08:02:47 -0500

    Added includes in tests

--- test/Json5UtilsTests.cpp
+++ test/Json5UtilsTests.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
 
+#include "include/json5/json5.hpp"
+#include "include/json5/json5_input.hpp"
+#include "include/json5/json5_output.hpp"
+#include "include/json5/json5_reflect.hpp"
+
 using namespace std::string_literals;
 using namespace std::string_view_literals;
 namespace fs = std::filesystem;
